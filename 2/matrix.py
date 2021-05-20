01b5c8c9fc7f6978e8a28a77d149a9755250440b
k-810 <k810marina@gmail.com>
2021-05-21 05:36:29 +0900

    matrix.py 計算

---
+++ 2/matrix.py
@@ -0,0 +1,45 @@
+import numpy
+import sys
+import time
+
+if (len(sys.argv) != 2):
+    print("usage: python %s N" % sys.argv[0])
+    quit()
+
+n = int(sys.argv[1])
+a = numpy.zeros((n, n))  # Matrix A
+b = numpy.zeros((n, n))  # Matrix B
+c = numpy.zeros((n, n))  # Matrix C
+
+# Initialize the matrices to some values.
+for i in range(n):
+    for j in range(n):
+        a[i, j] = i * n + j
+        b[i, j] = j * n + i
+        c[i, j] = 0
+
+begin = time.time()
+
+
+def calc_matrix_product(a, b, c, n):
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                c[i, j] += a[i, k] * b[k, j]
+    return c
+
+
+calc_matrix_product(a, b, c, n)
+
+end = time.time()
+print("time: %.6f sec" % (end - begin))
+
+# Print C for debugging. Comment out the print before measuring the execution time.
+total = 0
+for i in range(n):
+    for j in range(n):
+        # print c[i, j]
+        total += c[i, j]
+# Print out the sum of all values in C.
+# This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
+print("sum: %.6f" % total)
